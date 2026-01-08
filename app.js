239d500ef236c9384b2c9b5f1478b4394645acad
amanak7 <22je0094@iitism.ac.in>
2026-01-08 18:42:22 +0530

    add dockerfile

--- app.js
+++ app.js
@@ -4,24 +4,29 @@ import { connectDB } from "./config/connectDB.js";
 import authRoutes from "./auth/route.js";
 import todoRoutes from "./todo/route.js";
 import passport from "./config/passport.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
 dotenv.config();
 const PORT = process.env.PORT;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+
 app.use(passport.initialize());
 
 connectDB();
 
 app.get("/",(req,res)=>{
-    res.json({
-        status:true,
-        message:`Server is running on port ${PORT}`
-    })
+    res.render("index");
 })
 
 app.use("/api/v1/auth",authRoutes);
